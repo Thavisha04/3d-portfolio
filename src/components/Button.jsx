d590ace6268d22f014e61ef2d7327b476e6db2bc
Thavisha04 <thavisha.thanippuliappuhamilag@dcmail.ca>
2026-02-04 02:01:04 -0500

    Created introduction and experience section

---
+++ src/components/Button.jsx
@@ -0,0 +1,29 @@
+const Button = ({text, classname, id}) => {
+    return (
+        <a
+            onClick={(e) => {
+                e.preventDefault();
+
+                const target = document.getElementById('counter');
+
+                if(target && id) {
+                    const offset = window.innerHeight * 0.15;
+
+                    const top = target.getBoundingClientRect().top + window.scrollY - offset;
+
+                    window.scrollTo({ top, behavior: 'smooth' });
+                }
+            }}
+            className={`${classname ?? ''} cta-wrapper`}>
+            <div className="cta-button group">
+                <div className="bg-circle" />
+                <p className="text">See my Work</p>
+                <div className="arrow-wrapper">
+                    <img src="/images/arrow-down.svg" alt="arrow" />
+                </div>
+            </div>
+        </a>
+    )
+}
+
+export default Button;
---
+++ src/sections/ShowcaseSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ShowcaseSection = () => {
+    return (
+        <div id="work" className="app-showcase">
+            <div className="w-full">
+                <div className="showcaselayout"></div>
+            </div>
+        </div>
+    )
+}
+
+export default ShowcaseSection;
